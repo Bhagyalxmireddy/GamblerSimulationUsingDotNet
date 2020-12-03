d6721ffe488d98174c8d3fb917cf9af0271692aa
Bhagi <puppyreddy1997@gmail.com>
2020-12-03 11:04:26 +0530

    [Bhagyalaxmi]ADDED:calucating the gamblerStake for 50% to winning or lossing

--- GamblerSimulation/Program.cs
+++ GamblerSimulation/Program.cs
@@ -5,7 +5,10 @@ namespace GamblerSimulation
     class Program
     {
         public const int STAKE = 100;
+        public const int PERCENT = 50;
         public const int BET = 1;
+        int winning = 0;
+        int lossing = 0;
         int stakeValue;
         int result;
         static void Main(string[] args)
@@ -16,6 +19,11 @@ namespace GamblerSimulation
         }
         public  int gamblerGame()
         {
+            int value = (STAKE * PERCENT)/100;
+            winning = STAKE + value;
+            Console.WriteLine("The Winning value of gambler is : " + winning);
+            lossing = STAKE - value;
+            Console.WriteLine("The Lossing value of gambler is : " + lossing);
             Random random = new Random();
             int bettingToss = random.Next(0, 2);
             if(bettingToss == 1)
