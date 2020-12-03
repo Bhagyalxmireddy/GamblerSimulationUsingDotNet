d1c0c514d7e6537a616807e14ac535d2fbe32de9
Bhagi <puppyreddy1997@gmail.com>
2020-12-03 10:06:11 +0530

    [Bhagyalaxmi]ADDED:Intializing the variables

--- GamblerSimulation/Program.cs
+++ GamblerSimulation/Program.cs
@@ -4,6 +4,8 @@ namespace GamblerSimulation
 {
     class Program
     {
+        public const int STAKE = 100;
+        public const int BET = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to GamblerSimulation program");
