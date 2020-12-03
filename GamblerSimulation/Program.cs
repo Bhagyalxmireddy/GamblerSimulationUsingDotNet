968235eb0a2c347764cd5b33bd452a9fa4531ae9
Bhagi <puppyreddy1997@gmail.com>
2020-12-03 13:15:11 +0530

    [Bhagyalaxmi]Refactered:Refacted code calculate the won and loss days

--- GamblerSimulation/Program.cs
+++ GamblerSimulation/Program.cs
@@ -31,34 +31,35 @@ namespace GamblerSimulation
             Console.WriteLine("The Winning value of gambler is : " + winning);
             lossing = STAKE - value;
             Console.WriteLine("The Lossing value of gambler is : " + lossing);
-                amount = 100;
-                while (amount > lossing && amount < winning)
+            
+            amount = 100;
+            while (amount > lossing && amount < winning)
+            {
+                Random random = new Random();
+                int bettingToss = random.Next(0, 2);
+                if (bettingToss == 1)
                 {
-                    Random random = new Random();
-                    int bettingToss = random.Next(0, 2);
-                    if (bettingToss == 1)
-                    {
-                        amount = amount + BET;
-                        Console.WriteLine("Gambler Won the Game : " + amount);
-                    }
-                    else
-                    {
-                        amount = amount - BET;
-                        Console.WriteLine("Gambler loss the Game : " + amount);
-                    }
-                }
-                if (amount == 150)
-                {
-                    winamount = winamount + amount;
-                    won = won + 1;
+                    amount = amount + BET;
+                    Console.WriteLine("Gambler Won the Game : " + amount);
                 }
                 else
                 {
-                    lossamount = amount - lossamount;
-                    loss = loss - 1;
+                    amount = amount - BET;
+                    Console.WriteLine("Gambler loss the Game : " + amount);
                 }
-                Console.WriteLine("amount of a day " + amount);
-                Console.WriteLine("amount of a day :" + days + "is: " + amount);
+            }
+            if (amount == 150)
+            {
+                winamount = winamount + amount;
+                won = won + 1;
+            }
+            else
+            {
+                lossamount = amount - lossamount;
+                loss = loss - 1;
+            }
+            Console.WriteLine("amount of a day " + amount);
+            Console.WriteLine("amount of a day :" + days + "is: " + amount);
             stakeValue = stakeValue + amount;
             return result;
         }
@@ -67,9 +68,29 @@ namespace GamblerSimulation
             for (days = 1; days <= DAYS; days++)
             {
                 gamblerGame();
-                
-            }           
+
+            }
             Console.WriteLine("Total amount used during the game : " + stakeValue);
+            Console.WriteLine("Totalamount won after a month " + winamount);
+            Console.WriteLine("Totalamount loss after a month " + lossamount);
+            int  totalAmount = STAKE * DAYS;
+            Console.WriteLine("Totalamount is " + totalAmount);
+            if (totalAmount > stakeValue)
+            {
+                int amountLoss = totalAmount - stakeValue;
+                Console.WriteLine("amount loss is :" + amountLoss);
+            }
+            else if(totalAmount == stakeValue)
+            {
+                Console.WriteLine("No amount is loss or win");
+            }
+            else
+            {
+                int amountWon = stakeValue - totalAmount;
+                Console.WriteLine("amount won is :" + amountWon);
+            }
+            Console.WriteLine("no.of won days is " + won);
+            Console.WriteLine("no.of loss days is :" + loss);
             return result;
         }
     }
