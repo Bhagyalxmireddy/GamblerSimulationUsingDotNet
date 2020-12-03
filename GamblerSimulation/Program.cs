52d2359dcb425e197d2a493d5d73f36d2e091c5c
Bhagi <puppyreddy1997@gmail.com>
2020-12-03 10:26:45 +0530

    [Bhagyalaxmi]ADDED:Added method to calculate the gambler win or loss by using Random

--- GamblerSimulation/Program.cs
+++ GamblerSimulation/Program.cs
@@ -6,9 +6,31 @@ namespace GamblerSimulation
     {
         public const int STAKE = 100;
         public const int BET = 1;
+        int loss = 0;
+        int win = 0;
+        int stakeValue;
+        int result;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to GamblerSimulation program");
+            Program gambler = new Program();
+            gambler.gamblerGame();
+        }
+        public  int gamblerGame()
+        {
+            Random random = new Random();
+            int bettingToss = random.Next(0, 2);
+            if(bettingToss == 1)
+            {
+                stakeValue = STAKE + BET;
+                Console.WriteLine("Gambler Won the Game" + stakeValue);
+            }
+            else
+            {
+                stakeValue = STAKE - BET;
+                Console.WriteLine("Gambler loss the Game" + stakeValue);
+            }
+            return result;
         }
     }
 }
